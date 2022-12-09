d480b57c5ccbdb816ff5db6dd1fac434f47fee98
DaviBraz7 <davisouzabraz@gmail.com>
2022-12-08 23:55:43 -0300

    Main

---
+++ NotificationContext/Notifiable.cs
@@ -0,0 +1,7 @@
+namespace Balta.NotificationContext
+{
+    public class Notifiable
+    {
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Balta.ContentContext;
 
 namespace Balta 
@@ -18,6 +19,37 @@ namespace Balta
                 Console.WriteLine(article.Title);
                 Console.WriteLine(article.Url);
             }
+
+            var courses = new List<Course>();
+            var courseOOP = new Course("Fudamentos OOP", "fundamentos-oop");
+            var courseCSharp = new Course("Fudamentos C#", "fundamentos-csharp");
+            var courseAspNet = new Course("Fudamentos ASP.NET", "fundamentos-aspdotnet");
+
+            courses.Add(courseOOP);
+            courses.Add(courseCSharp);
+            courses.Add(courseAspNet);
+
+            var careers = new List<Career>();
+            var careerDotNet = new Career("Especialista .NET", "especialista-DotNet");
+            var careerItem2 = new CareerItem(2, "aprenda OOP", "", courseOOP);
+            var careerItem = new CareerItem(1, "Comece por aqui", "", courseCSharp);
+            var careerItem3 = new CareerItem(3, "aprenda .NET", "", courseAspNet);
+            careerDotNet.Items.Add(careerItem2);
+            careerDotNet.Items.Add(careerItem3);
+            careerDotNet.Items.Add(careerItem);
+            careers.Add(careerDotNet);
+
+            foreach (var career in careers)
+            {
+                Console.WriteLine(career.Title);
+                foreach (var item in career.Items.OrderBy(x => x.Order))
+                {
+                    Console.WriteLine($"{item.Order} - {item.Title}");
+                    Console.WriteLine(item.Course.Title);
+                    Console.WriteLine(item.Course.Level);
+                }
+            }
+
         }
     }
 }
